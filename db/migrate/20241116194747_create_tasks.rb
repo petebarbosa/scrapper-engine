af654a91c4f4064b893f97c6defa1f01203e782f
petebarbosa <pedrovbb89@gmail.com>
2024-11-17 15:21:39 -0300

    feat: add error_message and change positions, just because don't worrky about it

--- db/migrate/20241116194747_create_tasks.rb
+++ db/migrate/20241116194747_create_tasks.rb
@@ -1,10 +1,13 @@
 class CreateTasks < ActiveRecord::Migration[7.2]
   def change
     create_table :tasks do |t|
-      t.string :url_to_scrape
       t.references :user, null: false, foreign_key: true
-      t.integer :status
+      t.string :name
+      t.string :description
+      t.string :url_to_scrape, null: false
+      t.integer :status, default: 0
       t.json :scraped_data
+      t.text :error_message
 
       t.timestamps
     end
